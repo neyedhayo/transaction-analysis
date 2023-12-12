a07ca90e00c799628f8ea85230703da27487a9fd
neyedhayo <0xmaxlord@gmail.com>
2023-12-12 19:19:45 +0100

    updated script

--- scripts/app.py
+++ scripts/app.py
@@ -1,25 +1,31 @@
 import gradio as gr
 import pandas as pd
-from visualization import visualize_users_exceeding_50, isualize_highest_monthly_transaction_user
+from visualization import visualize_users_exceeding_50, visualize_highest_monthly_transaction_user
 
 
 def visualize(files):
     # Read data from each file using pandas
-    dataframes = []
-    for f in files:
-        data = pd.read_csv(f)
+    # dataframes = []
+    # for f in files:
+        # data = pd.read_csv(f)
 
-        # Data cleaning steps
-        transaction_data = data[['account_id', 'amount', 'status', 'created_at']]
-        transaction_data['created_at'] = pd.to_datetime(transaction_data['created_at'], format = '%d/%m/%Y %H:%M')
-        transaction_data = transaction_data.rename(columns={'account_id': 'AccountID', 'amount': 'TransactionAmount', 'status': 'Status', 'created_at': 'DatePeriod'}) #.rename_axis('DateTime')
-        transaction_data.set_index('created_at', inplace=True)
+        # # Data cleaning steps
+        # transaction_data = data[['account_id', 'amount', 'status', 'created_at']]
+        # transaction_data['created_at'] = pd.to_datetime(transaction_data['created_at'], format = '%d/%m/%Y %H:%M')
+        # transaction_data = transaction_data.rename(columns={'account_id': 'AccountID', 'amount': 'TransactionAmount', 'status': 'Status', 'created_at': 'DatePeriod'}) #.rename_axis('DateTime')
+        # transaction_data.set_index('created_at', inplace=True)
+    data = pd.read_csv(files)
 
-        dataframes.append(transaction_data)
+    # Data cleaning steps
+    transaction_data = data[['account_id', 'amount', 'status', 'created_at']]
+    transaction_data['created_at'] = pd.to_datetime(transaction_data['created_at'], format = '%d/%m/%Y %H:%M')
+    transaction_data = transaction_data.rename(columns={'account_id': 'AccountID', 'amount': 'TransactionAmount', 'status': 'Status', 'created_at': 'DatePeriod'}) #.rename_axis('DateTime')
+    transaction_data.set_index('created_at', inplace=True)
+    # dataframes.append(transaction_data)
 
     # Function call of visualization
-    vis1 = visualize_users_exceeding_50(dataframes[0])
-    vis2 = visualize_highest_monthly_transaction_user(dataframes[0])
+    vis1 = visualize_users_exceeding_50(transaction_data[0])
+    vis2 = visualize_highest_monthly_transaction_user(transaction_data[0])
 
     return vis1, vis2
 
